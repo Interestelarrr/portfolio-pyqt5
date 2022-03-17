30fd2f1e8c3dcd6350c45ec3ac060297f3f896c5
drevenzz <78106658+drevenzz@users.noreply.github.com>
2022-03-17 01:27:53 -0300

    Delete main.sh

--- main.sh
+++
@@ -1,7 +0,0 @@
-echo "# portfolio-pyqt5" >> README.md
-git init
-git add .
-git commit -m "Proyectos subidos"
-git branch -M master
-git remote add origin https://github.com/drevenzz/portfolio-pyqt5.git
-git push -u origin master
